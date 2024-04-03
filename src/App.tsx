22d0acf1a84bb93413b1e8630b51bc10dbc80f48
NAVI1322 <rksharma041976@gmail.com>
2024-04-03 15:24:49 -0400

    toggle button done

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,17 @@ import { AppBar } from './comps/AppBar'
 import { Bottom } from './comps/Bottom'
 
 import { Table } from './comps/Table'
+import { ToggleDarklight } from './comps/Toggle_darklight'
 
 
 function App() {
   return (
     <div>
-     
-     <AppBar />                                                                                                         
+    
+     <AppBar />                                        
+     <div className="flex justify-center">   <ToggleDarklight /></div>                                                                 
    <Table />
+ 
    <div className="fixed bottom-0 w-full flex justify-center sm:justify-center  font-extralight bg-gradient-to-b from-transparent to-tablerowgrey ">
   <Bottom />
 </div>
