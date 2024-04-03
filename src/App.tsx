d2473e64f94818b9d7660af291070c7b8969b153
Gurtaj06 <sonicgurtaj079@gmail.com>
2024-04-03 18:10:05 -0400

    update app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import { Button } from "./components/button"
-
+import Button from './components/button'
 import './App.css'
+import { EnterIcon } from '@radix-ui/react-icons'
 
-import { Table } from './comps/Table'
-
-
+function handleClick() {
+  console.log("Clicked")
+}
 function App() {
   return (
     <div>
-      <Table />
-
+      <Button onClick={handleClick} variant='primary'>Login<EnterIcon className='w-5 h-full' /></Button>
+      <Button onClick={handleClick} variant='primary-disabled'>Login<EnterIcon className='w-5 h-full' /></Button>
+      <Button onClick={handleClick} variant='secondary'>Login<EnterIcon className='w-5 h-full' /></Button>
+      <Button onClick={handleClick} variant='secondary-disabled'>Login<EnterIcon className='w-5 h-full' /></Button>
     </div>
 
   )
