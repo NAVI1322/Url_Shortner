35f0b6bf92d49f931a9e6f12333ad4e86f32a769
NAVI1322 <rksharma041976@gmail.com>
2024-04-14 00:10:53 -0400

    light and dark mode

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,29 +4,27 @@ import Login from "./pages/login";
 import Register from "./pages/register";
 import { useState } from "react";
 
-
-
 function App() {
   const [isChecked, setIsChecked] = useState(true);
-  const[theme, settheme] = useState(true);
+  const [theme, setTheme] = useState(true);
+
   const handleCheckboxChange = () => {
     setIsChecked(!isChecked);
-    settheme(!theme);
-  }
-  
-  return (
-    <div className={`html ${theme && "dark"}`}>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingPage handleCheckboxChange={handleCheckboxChange} isChecked={isChecked} theme={theme} />}></Route>
-        <Route path="/dashboard" element={<LandingPage handleCheckboxChange={handleCheckboxChange} isChecked={isChecked} theme={theme} />}></Route>
-        <Route path="/register" element={<Register />}></Route>
-        <Route path="/login" element={<Login />}></Route>
-      </Routes>
-    </BrowserRouter>
+    setTheme(!theme);
+  };
 
+  return (
+    <div className={`html ${theme ? "dark" : "light"}`}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage handleCheckboxChange={handleCheckboxChange} isChecked={isChecked} theme={theme} />}></Route>
+          <Route path="/dashboard" element={<LandingPage handleCheckboxChange={handleCheckboxChange} isChecked={isChecked} theme={theme} />}></Route>
+          <Route path="/register" element={<Register />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
 export default App;
